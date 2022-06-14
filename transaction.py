3ccbea3e048ef67ae0ae16f4f53f87aa36b4dd30
Maltstar <101098152+Maltstar@users.noreply.github.com>
2022-06-14 11:38:00 +0200

    handling empty response for wallet address

--- transaction.py
+++ transaction.py
@@ -5,6 +5,9 @@ def TransactionDecoder(transaction):
 
 class Transaction:
     def __init__(self,transaction_dict):
-        # convert all key/values pair into properties of the class
-        for key in transaction_dict:
-            setattr(self, key, transaction_dict[key])
+        #print('Transaction', str(type(transaction_dict)))
+        #in case the response fetched is empty do not parse it
+        if str(type(transaction_dict)) != "<class 'NoneType'>":
+            # convert all key/values pair into properties of the class
+            for key in transaction_dict:
+                setattr(self, key, transaction_dict[key])
