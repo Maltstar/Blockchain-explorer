107f165ce817c09c6667abe54beb4862294a6438
Maltstar <hftool@proton.me>
2024-08-08 01:28:05 +0200

    adding git ignore and config for starting app

--- app.py
+++ app.py
@@ -121,4 +121,6 @@ def address(coin,wallet_address):
 
 # Server starten
 #app.run()
-serve(app, host="127.0.0.1", port=5000)
+#serve(app, host="127.0.0.1", port=5000)
+if __name__ == '__main__':
+    app.run()
